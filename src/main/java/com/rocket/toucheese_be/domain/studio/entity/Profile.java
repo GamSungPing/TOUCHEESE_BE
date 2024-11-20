13c343eb4aa732807230207ed68f0c0f73e2d402
juyoon <juyoon0423@daum.net>
2024-11-20 14:40:28 +0900

    Fix: 엔티티 추가

---
+++ src/main/java/com/rocket/toucheese_be/domain/studio/entity/Profile.java
@@ -0,0 +1,26 @@
+package com.rocket.toucheese_be.domain.studio.entity;
+
+import jakarta.persistence.*;
+        import lombok.*;
+
+        import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Builder
+@AllArgsConstructor(access = PROTECTED)
+@NoArgsConstructor(access = PROTECTED)
+@Getter
+@ToString(callSuper = false)
+public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String profileURL;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "studio_id")
+    private Studio studio;
+
+}
+
