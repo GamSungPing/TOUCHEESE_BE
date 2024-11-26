88e248af676fb0060d076fb7e093cfa08e4817b8
harriet221 <potter19@dongguk.edu>
2024-11-26 23:24:00 +0900

    Refactor: concept 조회 메서드에 swagger 설명 추가

--- src/main/java/com/rocket/toucheese_be/domain/studio/controller/NotStudioController.java
+++ src/main/java/com/rocket/toucheese_be/domain/studio/controller/NotStudioController.java
@@ -5,6 +5,7 @@ import com.rocket.toucheese_be.domain.studio.entity.Concept;
 import com.rocket.toucheese_be.domain.studio.service.NotStudioService;
 import com.rocket.toucheese_be.global.response.Response;
 import com.rocket.toucheese_be.global.response.SuccessCode;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,14 +23,16 @@ public class NotStudioController {
 
     private final NotStudioService service;
 
-    @GetMapping("concepts")
+    @Operation(summary = "컨셉 목록 조회", description = "컨셉 목록을 조회합니다.")
+    @GetMapping("/concepts")
     public Response<List<ConceptListDto>> getAllConcept() {
         List<Concept> conceptList = service.getAllConcept();
         List<ConceptListDto> conceptDtoList = ConceptListDto.fromConceptList(conceptList);
         return Response.of(SuccessCode.GET_CONCEPT_LIST_SUCCESS, conceptDtoList);
     }
 
-    @GetMapping("concept/{id}")
+    @Operation(summary = "특정 컨셉 조회", description = "컨셉 Id를 통해 해당 컨셉을 조회합니다.")
+    @GetMapping("/concept/{id}")
     public Response<ConceptListDto> getConcept(@PathVariable("id") Long id) {
         Concept concept = service.getConceptById(id);
         ConceptListDto conceptDtoList = new ConceptListDto(concept);
