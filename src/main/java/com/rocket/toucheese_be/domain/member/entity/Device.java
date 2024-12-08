8b82e01269c5da2a1c06e0fd5a1d2eb2c5a28c89
harriet221 <potter19@dongguk.edu>
2024-12-09 00:00:44 +0900

    Fix: 연관관계 맵핑 오류 수정

--- src/main/java/com/rocket/toucheese_be/domain/member/entity/Device.java
+++ src/main/java/com/rocket/toucheese_be/domain/member/entity/Device.java
@@ -3,18 +3,21 @@ package com.rocket.toucheese_be.domain.member.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
+@RequiredArgsConstructor
 @Getter
 @Setter
 @Entity
+@Builder
 public class Device {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long memberId;
+    // Member 클래스의 device 필드가 외래 키 소유 / 관리
+    @OneToOne(mappedBy = "device")
+    private Member member;
 
     private String deviceToken;  // FCM 디바이스 토큰
 }
