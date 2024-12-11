81bf2dc88bab90a18724c4b6bf493da2ccd94429
harriet221 <potter19@dongguk.edu>
2024-12-11 15:23:46 +0900

    Refactor: 토큰 등록 및 갱신 로직 서비스로 분리, 기존 토큰에 대해 토큰 값이 다른 경우만 갱신하도록 수정

--- src/main/java/com/rocket/toucheese_be/domain/member/controller/DeviceController.java
+++ src/main/java/com/rocket/toucheese_be/domain/member/controller/DeviceController.java
@@ -2,13 +2,8 @@ package com.rocket.toucheese_be.domain.member.controller;
 
 import com.rocket.toucheese_be.domain.member.dto.DeviceRegisterDto;
 import com.rocket.toucheese_be.domain.member.entity.Device;
-import com.rocket.toucheese_be.domain.member.entity.Member;
 import com.rocket.toucheese_be.domain.member.service.DeviceService;
-import com.rocket.toucheese_be.domain.member.service.MemberService;
-import com.rocket.toucheese_be.global.response.CustomException;
-import com.rocket.toucheese_be.global.response.ErrorCode;
 import com.rocket.toucheese_be.global.response.Response;
-import com.rocket.toucheese_be.global.response.SuccessCode;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,47 +11,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-
 @RequestMapping("/api/v1/device")
 @RequiredArgsConstructor
 @RestController
 public class DeviceController {
     private final DeviceService deviceService;
-    private final MemberService memberService;
 
     // 푸시 알림에 대해 사용자가 승낙할 때 호출되어야 함 (디바이스 토큰 등록)
-    // TODO: 토큰 유효성 체크해서 갱신하는 로직 필요
-    // 1. 타임 스탬프 - 사용 시마다 타임 스탬프 갱신, 미사용 100일 경과 후 삭제
-    // 2. 정기적으로 클라이언트에서 토큰 유효성 검사해서 서버에 결과 전송
     // 한 멤버가 하나의 디바이스만 가지는 것으로 가정
-    // 아직) 토큰 무효화 조건 1)앱 삭제 2)토큰 기간 만료(100일로 가정) 3)앱 데이터 초기화(캐시 및 저장 데이터 삭제)
     @Operation(
             summary = "디바이스 토큰 등록",
             description = "푸시 알림에 대해 사용자가 승낙할 때 DeviceRegisterDto와 함께 호출되어야 합니다")
     @PostMapping("/register")
     public Response<Device> registerDevice(@RequestBody DeviceRegisterDto deviceRegisterDto) {
-        Member member = memberService.findById(deviceRegisterDto.memberId())
-                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_MEMBER));
-
-        Optional<Device> existingDevice = Optional.ofNullable(member.getDevice());
-
-        if (existingDevice.isPresent()) {
-            // 해당 멤버에 device token 이미 있는 경우
-            Device device = existingDevice.get();
-            device.setDeviceToken(deviceRegisterDto.deviceToken()); // 토큰 갱신
-            deviceService.save(device);
-            return Response.of(SuccessCode.UPDATE_DEVICE_TOKEN_SUCCESS);
-        } else {
-            // 새로 만드는 경우
-            Device device = Device.builder()
-                    .member(member)
-                    .deviceToken(deviceRegisterDto.deviceToken())
-                    .build();
-            deviceService.save(device);
-            member.setDevice(device);
-            memberService.save(member);
-            return Response.of(SuccessCode.GET_DEVICE_TOKEN_SUCCESS);
-        }
+        return deviceService.registerDevice(deviceRegisterDto);
     }
 }
