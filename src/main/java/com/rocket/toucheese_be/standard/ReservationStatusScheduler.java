830abf86d6aef367bfaf768e2a268e575d693c3b
juyoon <juyoon0423@daum.net>
2024-12-13 14:53:23 +0900

    Refactor: 스케줄러 예약확정 -> 완료 상태도 추가

--- src/main/java/com/rocket/toucheese_be/standard/ReservationStatusScheduler.java
+++ src/main/java/com/rocket/toucheese_be/standard/ReservationStatusScheduler.java
@@ -19,13 +19,19 @@ public class ReservationStatusScheduler {
 
     @Scheduled(cron = "0 0 */12 * * *") // 12시간마다 실행
     @Transactional
-    public void cancelPastReservations() {
+    public void updateReservationStatuses() {
         List<Reservation> reservations = reservationRepository.findAll();
 
         for (Reservation reservation : reservations) {
             // 예약 상태가 'waiting'이고, 예약 시간이 현재를 넘었으면 취소 처리
             if (reservation.getStatus() == ReservationStatus.waiting && isPastReservation(reservation)) {
                 reservation.cancel();
+                reservationRepository.save(reservation);  // 상태 변경 후 저장
+            }
+
+            // 예약 상태가 'confirm'이고, 예약 시간이 현재를 넘었으면 완료 처리
+            if (reservation.getStatus() == ReservationStatus.confirm && isPastReservation(reservation)) {
+                reservation.complete();
                 reservationRepository.save(reservation);  // 상태 변경 후 저장
             }
         }
@@ -37,4 +43,5 @@ public class ReservationStatusScheduler {
         return reservationDateTime.isBefore(LocalDateTime.now());
     }
 }
+
 
