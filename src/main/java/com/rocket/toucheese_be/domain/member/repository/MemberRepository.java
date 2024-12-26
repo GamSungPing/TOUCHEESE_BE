9aaa3430f410d3ed57a0e81630ad031967817e06
harriet221 <potter19@dongguk.edu>
2024-12-26 13:17:55 +0900

    Fix: 닉네임 변경 더티 체킹 방지 setter -> 쿼리 작성

--- src/main/java/com/rocket/toucheese_be/domain/member/repository/MemberRepository.java
+++ src/main/java/com/rocket/toucheese_be/domain/member/repository/MemberRepository.java
@@ -3,6 +3,9 @@ package com.rocket.toucheese_be.domain.member.repository;
 import com.rocket.toucheese_be.domain.member.entity.Member;
 import com.rocket.toucheese_be.domain.member.entity.SocialType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,4 +15,9 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findBySocialTypeAndSocialId(SocialType socialType, String socialId);
 
     boolean existsByName(String name);
+
+    // 더티 체킹 방지
+    @Modifying
+    @Query("UPDATE Member m SET m.name = :name WHERE m.id = :id")
+    void updateName(@Param("id") Long id, @Param("name") String name);
 }
